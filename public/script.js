43a595e9cd76ed36f5140abc88eff1800c0eab2a
Cara Canady <cara.canady@gmail.com>
2016-05-14 16:13:23 -0400

    including json file, will need to merge with nada

--- public/script.js
+++ public/script.js
@@ -2,13 +2,13 @@ var app = angular.module('myModule', []);
 
 app.controller('resultscontroller', function($http, $scope){
 
-  $scope.execute = function (){
+$scope.execute = function (){
     var arrays = [];
     $http({
       method: 'GET',
-      url: 'https://content.guardianapis.com/search?q=beyonce&api-key=5d741299-d8d4-4fbf-8309-f6e6049e531c'
+      url: '/json/jokes.json'
     }).then(function (response) {
-      $scope.arrays = response.data.response.results;
+      console.log(response);
       
     }, function () {
     // called asynchronously if an error occurs
@@ -17,5 +17,37 @@ app.controller('resultscontroller', function($http, $scope){
     });
   }
 });
+//   $scope.execute = function (){
+//     var arrays = [];
+//     $http({
+//       method: 'GET',
+//       url: 'https://content.guardianapis.com/search?q=beyonce&api-key=5d741299-d8d4-4fbf-8309-f6e6049e531c'
+//     }).then(function (response) {
+//       $scope.arrays = response.data.response.results;
+      
+//     }, function () {
+//     // called asynchronously if an error occurs
+//     // or server returns response with an error status.
+//       console.log("ERROR");
+//     });
+//   }
+// });
+// var app = angular.module('myModule', []);
 
-  	
+// app.controller('resultscontroller', function($http, $scope){
+
+//   $scope.execute = function (){
+//     var arrays = [];
+//     $http({
+//       method: 'GET',
+//       url: 'http(s)://api.sportradar.us/mlb-t1/games/[event_id]/boxscore.[format]?api_key=[your_api_key]'
+//     }).then(function (response) {
+//       console.log(response);    
+//     }, function () {
+//     // called asynchronously if an error occurs
+//     // or server returns response with an error status.
+//       console.log("ERROR");
+//     });
+//   }
+// });
+//   	
