cd8076949310be66105664563145286846e57e80
Nada Hassan <nadahassan013@gmail.com>
2016-05-19 20:33:24 -0400

    one controller again

--- public/module.js
+++ public/module.js
@@ -1,54 +1,70 @@
 var app = angular.module('myModule',[]);
-//
-//     app.controller('jobInterviewcontroller', function($scope, $http){
-//       $scope.break = function(){
-//
-//     if (document.getElementById("situation").value === "job"){
-//       var arrays = [];
-//       $http({
-//         method: 'GET',
-//         url: 'https://api.nytimes.com/svc/mostpopular/v2/mostviewed/world/1.json?&api-key=c272b43bf0ba4faa9817ba9eb353c6b4'
-//       }).then(function (response) {
-//     $scope.arrays = response.data.results;
-//     }, function (response) {
-//       console.log('error')
-//     });
-//       console.log('job id works')
-//     };
-//     };
-//     });
-//     app.controller('speechPrescontroller', function($scope, $http){
-//       $scope.break = function(){
-//
-//      if (document.getElementById("situation").value === "speech"){
-//       var arrays = [];
-//       $http({
-//         method: 'GET',
-//         url: 'https://api.nytimes.com/svc/mostpopular/v2/mostviewed/world/1.json?&api-key=c272b43bf0ba4faa9817ba9eb353c6b4'
-//       }).then(function (response) {
-//     $scope.arrays = response.data.results;
-//     }, function (response) {
-//       console.log('error')
-//     });
-//       console.log('speech id works')
-//
-//     };
-//   };
-//   });
-//   app.controller('elevatorSpeechcontroller', function($scope, $http){
-//     $scope.break = function(){
-//
-//      if (document.getElementById("situation").value === "elevator"){
-//       var arrays = [];
-//       $http({
-//         method: 'GET',
-//         url: 'https://api.nytimes.com/svc/mostpopular/v2/mostviewed/world/1.json?&api-key=c272b43bf0ba4faa9817ba9eb353c6b4'
-//       }).then(function (response) {
-//     $scope.arrays = response.data.results;
-//     }, function (response) {
-//       console.log('error')
-//     });
-//     console.log('elevator id works')
-//
-//     } else {
-//       console.log('error!')};
+
+app.controller('resultscontroller', function($scope, $http, $location){
+  $scope.break = function(){
+
+  if (document.getElementById("situation").value === "date"){
+      var arrays = [];
+      $http({
+        method: 'GET',
+        url: 'https://api.nytimes.com/svc/mostpopular/v2/mostviewed/world/1.json?&api-key=c272b43bf0ba4faa9817ba9eb353c6b4'
+      }).then(function (response) {
+    $scope.arrays = response.data.results;
+    var path = $location.path('/firstDate.html')
+    });
+      console.log("date id works")
+
+  if (document.getElementById("situation").value === "inlaws"){
+      var arrays = [];
+      $http({
+        method: 'GET',
+        url: 'https://api.nytimes.com/svc/mostpopular/v2/mostviewed/world/1.json?&api-key=c272b43bf0ba4faa9817ba9eb353c6b4'
+      }).then(function (response) {
+    $scope.arrays = response.data.results;
+    });
+      console.log("inlaws id works")
+
+  if (document.getElementById("situation").value === "mingle"){
+      var arrays = [];
+      $http({
+        method: 'GET',
+        url: 'https://api.nytimes.com/svc/mostpopular/v2/mostviewed/world/1.json?&api-key=c272b43bf0ba4faa9817ba9eb353c6b4'
+      }).then(function (response) {
+    $scope.arrays = response.data.results;
+    $location.url(mingling.html)
+    });
+      console.log("party id works")
+
+    if (document.getElementById("situation").value === "job"){
+      var arrays = [];
+      $http({
+        method: 'GET',
+        url: 'https://api.nytimes.com/svc/mostpopular/v2/mostviewed/world/1.json?&api-key=c272b43bf0ba4faa9817ba9eb353c6b4'
+      }).then(function (response) {
+    $scope.arrays = response.data.results;
+    });
+      console.log('job id works')
+
+     if (document.getElementById("situation").value === "speech"){
+      var arrays = [];
+      $http({
+        method: 'GET',
+        url: 'https://api.nytimes.com/svc/mostpopular/v2/mostviewed/world/1.json?&api-key=c272b43bf0ba4faa9817ba9eb353c6b4'
+      }).then(function (response) {
+    $scope.arrays = response.data.results;
+    });
+      console.log('speech id works')
+
+     if (document.getElementById("situation").value === "elevator"){
+      var arrays = [];
+      $http({
+        method: 'GET',
+        url: 'https://api.nytimes.com/svc/mostpopular/v2/mostviewed/world/1.json?&api-key=c272b43bf0ba4faa9817ba9eb353c6b4'
+      }).then(function (response) {
+    $scope.arrays = response.data.results;
+    });
+    console.log('elevator id works')
+  } else {
+      console.log('error!')};
+    };
+  });
