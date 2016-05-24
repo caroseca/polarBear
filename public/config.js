730925c9ca3d8a3e4bb3d4a0a318eb7e4909f00a
Cara Canady <cara.canady@gmail.com>
2016-05-23 23:55:17 -0400

    cleaned up config

--- public/config.js
+++ public/config.js
@@ -2,30 +2,6 @@ var app = angular.module('myModule', ['ngRoute']);
 
 app.config(function($routeProvider) {
 	$routeProvider
-		.when("/firstDate", {
-			controller: "firstDatecontroller",
-			templateUrl: "firstDate.html"
-				})
-		.when("/inlaws", {
-			controller: "inLawscontroller",
-			templateUrl: "inLaws.html"
-				})
-		.when("/mingling", {
-			controller: "minglingcontroller",
-			templateUrl: "mingling.html"
-				})
-		.when("/jobInterview", {
-			controller: "jobInterviewcontroller",
-			templateUrl: "jobInterview.html"
-				})
-		.when("/speechPres", {
-			controller: "speechPrescontroller",
-			templateUrl: "speechPres.html"
-				})
-		.when("/elevatorSpeech", {
-			controller: "elevatorSpeechcontroller",
-			templateUrl: "elevatorSpeech.html"
-				})
 		.when("/aboutUs", {
 			controller: "aboutUs",
 			templateUrl: "aboutUs.html"
