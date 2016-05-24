6e04e053654fff1057db16a1cd2702aca8b0d644
Cara Canady <cara.canady@gmail.com>
2016-05-23 22:18:19 -0400

    unified controller, working on the page. needs tweaking.

--- public/module.js
+++ public/module.js
@@ -2,68 +2,160 @@ var app = angular.module('myModule',[]);
 
 app.controller('resultscontroller', function($scope, $http, $location){
   $scope.break = function(){
-
+//////////////////////////
+// DATE IS POPCULTURE API + JOKES
+//////////////////////////
   if (document.getElementById("situation").value === "date"){
-      var arrays = [];
+      $http({
+        method: 'GET',
+        url: 'http://content.guardianapis.com/search?order-by=newest&q=beyonce&api-key=5d741299-d8d4-4fbf-8309-f6e6049e531c'
+      }).then(function (responseBey) {
+        var arraysBey = [];
+          console.log(responseBey.data.response.results);
+          $( "<h3>A few Beyonce headlines to break the icy lemonade:</h3>" ).prependTo( ".results1" );
+          $scope.arraysBey = responseBey.data.response.results; 
+      });
+
+      $http({
+        method: 'GET',
+        url: '/json/jokes.json'
+      }).then(function (response) {
+        var jokeObject = [];
+        // console.log(response.data.jokes);
+        for(var i = 0; i <= 2; i++){
+          jokeObject.push(response.data.jokes[Math.floor(Math.random() * response.data.jokes.length)]);
+            }
+        $( "<h3>A few jokes to break that ice:</h3>" ).prependTo( ".results2" );
+        $scope.yayJoke = jokeObject; 
+      });
+//////////////////////////
+// MEETING INLAWS IS NYT API + JOKES
+//////////////////////////
+  } else if (document.getElementById("situation").value === "inlaws") {
       $http({
         method: 'GET',
         url: 'https://api.nytimes.com/svc/mostpopular/v2/mostviewed/world/1.json?&api-key=c272b43bf0ba4faa9817ba9eb353c6b4'
+      }).then(function (responseNyt) {
+        var arraysNyt = [];
+        // console.log(responseNyt);
+        $( "<h3>A few world news New York Times headlines to break the ice:</h3>" ).prependTo( ".results1" );
+        $scope.arraysNyt = responseNyt.data.results; //display on page {{array.abstract}}
+    });
+    
+    $http({
+        method: 'GET',
+        url: '/json/jokes.json'
       }).then(function (response) {
-    $scope.arrays = response.data.results;
-    var path = $location.path('/firstDate.html');
+        var jokeObject = [];
+        // console.log(response.data.jokes);
+        for(var i = 0; i <= 2; i++){
+          jokeObject.push(response.data.jokes[Math.floor(Math.random() * response.data.jokes.length)]);
+            }
+        $( "<h3>A few jokes to break that ice:</h3>" ).prependTo( ".results2" );
+        $scope.yayJoke = jokeObject; 
+      });
+  
+//////////////////////////
+// MINGLING IS BEYONCE + JOKES
+//////////////////////////
+} else if (document.getElementById("situation").value === "mingle"){
+      $http({
+        method: 'GET',
+        url: 'http://content.guardianapis.com/search?order-by=newest&q=beyonce&api-key=5d741299-d8d4-4fbf-8309-f6e6049e531c'
+      }).then(function (responseBey) {
+        var arraysBey = [];
+          console.log(responseBey.data.response.results);
+          $( "<h3>A few Beyonce headlines to break the icy lemonade:</h3>" ).prependTo( ".results1" );
+          $scope.arraysBey = responseBey.data.response.results; 
+      });
+
+      $http({
+        method: 'GET',
+        url: '/json/jokes.json'
+      }).then(function (response) {
+        var jokeObject = [];
+        // console.log(response.data.jokes);
+        for(var i = 0; i <= 2; i++){
+          jokeObject.push(response.data.jokes[Math.floor(Math.random() * response.data.jokes.length)]);
+            }
+        $( "<h3>A few jokes to break that ice:</h3>" ).prependTo( ".results2" );
+        $scope.yayJoke = jokeObject; 
+      });
+//////////////////////////
+// INTERVIEW IS NYT + GUARDIAN TECH
+//////////////////////////
+} else if (document.getElementById("situation").value === "job"){
+      $http({
+        method: 'GET',
+        url: 'https://api.nytimes.com/svc/mostpopular/v2/mostviewed/world/1.json?&api-key=c272b43bf0ba4faa9817ba9eb353c6b4'
+      }).then(function (responseNyt) {
+        var arraysNyt = [];
+        // console.log(responseNyt);
+        $( "<h3>A few world news New York Times headlines to break the ice:</h3>" ).prependTo( ".results1" );
+        $scope.arraysNyt = responseNyt.data.results; //display on page {{array.abstract}}
     });
 
-  } if (document.getElementById("situation").value === "inlaws"){
-      var arrays = [];
       $http({
         method: 'GET',
-        url: 'https://api.nytimes.com/svc/mostpopular/v2/mostviewed/world/1.json?&api-key=c272b43bf0ba4faa9817ba9eb353c6b4'
+        url: 'http://content.guardianapis.com/search?order-by=newest&section=technology&api-key=5d741299-d8d4-4fbf-8309-f6e6049e531c'
+      }).then(function (responseTech) {
+        var arraysTech = [];
+          console.log(responseTech.data.response.results);
+          $( "<h3>A few technology headlines to break the ice:</h3>" ).prependTo( ".results2" );
+          $scope.arraysTech = responseTech.data.response.results; 
+      });
+//////////////////////////
+// SPEECH IS JOKES AND TECH
+//////////////////////////
+} else if (document.getElementById("situation").value === "speech"){
+      $http({
+        method: 'GET',
+        url: '/json/jokes.json'
       }).then(function (response) {
-    $scope.arrays = response.data.results;
-    });
-      console.log("inlaws id works");
+        var jokeObject = [];
+        // console.log(response.data.jokes);
+        for(var i = 0; i <= 2; i++){
+          jokeObject.push(response.data.jokes[Math.floor(Math.random() * response.data.jokes.length)]);
+            }
+        $( "<h3>A few jokes to break that ice:</h3>" ).prependTo( ".results2" );
+        $scope.yayJoke = jokeObject; 
+      });
 
-} if (document.getElementById("situation").value === "mingle"){
-      var arrays = [];
       $http({
         method: 'GET',
-        url: 'https://api.nytimes.com/svc/mostpopular/v2/mostviewed/world/1.json?&api-key=c272b43bf0ba4faa9817ba9eb353c6b4'
-      }).then(function (response) {
-    $scope.arrays = response.data.results;
-    $location.url(mingling.html)
-    });
-      console.log("party id works");
+        url: 'http://content.guardianapis.com/search?order-by=newest&section=technology&api-key=5d741299-d8d4-4fbf-8309-f6e6049e531c'
+      }).then(function (responseTech) {
+        var arraysTech = [];
+          console.log(responseTech.data.response.results);
+          $( "<h3>A few technology headlines to break the ice:</h3>" ).prependTo( ".tech" );
+          $scope.arraysTech = responseTech.data.response.results; 
+      });
 
-}    if (document.getElementById("situation").value === "job"){
-      var arrays = [];
-      $http({
+//////////////////////////
+// ELEVATOR IS TECH + POPCULTURE
+//////////////////////////
+} else if (document.getElementById("situation").value === "elevator"){
+       
+       $http({
         method: 'GET',
-        url: 'https://api.nytimes.com/svc/mostpopular/v2/mostviewed/world/1.json?&api-key=c272b43bf0ba4faa9817ba9eb353c6b4'
-      }).then(function (response) {
-    $scope.arrays = response.data.results;
-    });
-      console.log('job id works');
+        url: 'http://content.guardianapis.com/search?order-by=newest&q=beyonce&api-key=5d741299-d8d4-4fbf-8309-f6e6049e531c'
+      }).then(function (responseBey) {
+        var arraysBey = [];
+          console.log(responseBey.data.response.results);
+          $( "<h3>A few Beyonce headlines to break the icy lemonade:</h3>" ).prependTo( ".results1" );
+          $scope.arraysBey = responseBey.data.response.results; 
+         });
 
-    } if (document.getElementById("situation").value === "speech"){
-      var arrays = [];
       $http({
         method: 'GET',
-        url: 'https://api.nytimes.com/svc/mostpopular/v2/mostviewed/world/1.json?&api-key=c272b43bf0ba4faa9817ba9eb353c6b4'
-      }).then(function (response) {
-    $scope.arrays = response.data.results;
-    });
-      console.log('speech id works');
-
-    } if (document.getElementById("situation").value === "elevator"){
-      var arrays = [];
-      $http({
-        method: 'GET',
-        url: 'https://api.nytimes.com/svc/mostpopular/v2/mostviewed/world/1.json?&api-key=c272b43bf0ba4faa9817ba9eb353c6b4'
-      }).then(function (response) {
-    $scope.arrays = response.data.results;
-    });
-    console.log('elevator id works')
+        url: 'http://content.guardianapis.com/search?order-by=newest&section=technology&api-key=5d741299-d8d4-4fbf-8309-f6e6049e531c'
+      }).then(function (responseTech) {
+        var arraysTech = [];
+          // console.log(responseTech.data.response.results);
+          $( "<h3>A few technology headlines to break the ice:</h3>" ).prependTo( ".results2" );
+          $scope.arraysTech = responseTech.data.response.results; 
+      });
   } else {
-      console.log('error!')};
+      console.log('Oh darn.  Trapped on an iceberg all by yourself!')};
     };
   });
